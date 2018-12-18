81c6d06c4f3d18a6f9730c50a2dd2e5f9ffadc84
Julia Romanenko <rja-97@yandex.ru>
2018-12-18 17:17:34 +0300

    added header

--- matter/src/component/Header.jsx
+++ matter/src/component/Header.jsx
@@ -3,51 +3,62 @@ import './Header.css';
 
 function Header() {
     return <div className='header'>
-        <span className='logo'></span>
-        <ul className='nav'>
-            <li className='nav__item'>shop
-                <ul className='menu'>
-                    <li className='menu-item'>Apparels
-                        <ul className='submenu'>
-                            <li className='submenu__item'>pants</li>
-                            <li className='submenu__item'>jumpsuits</li>
-                            <li className='submenu__item'>shorts</li>
-                            <li className='submenu__item'>tops</li>
-                        </ul>
-                    </li>
-                    <li className='menu-item'>Accessories
-                        <ul className='submenu'>
-                            <li className='submenu__item'>necklaces</li>
-                            <li className='submenu__item'>scarfs</li>
-                        </ul>
-                    </li>
-                    <li className='menu-item'>Other
-                        <ul className='submenu'>
-                            <li className='submenu__item'>houseware</li>
-                            <li className='submenu__item'>gift Cards</li>
-                        </ul>
-                    </li>
-                    <li className='menu-item'>Shop by collection
-                        <ul className='submenu'>
-                            <li className='submenu__item'>sale/Last Call</li>
-                            <li className='submenu__item'>essentials</li>
-                        </ul>
-                    </li>
-                    <li className='menu-item'>Shop by techniques
-                        <ul className='submenu'>
-                            <li className='submenu__item'>block Printing</li>
-                            <li className='submenu__item'>ikat</li>
-                            <li className='submenu__item'>jamdani</li>
-                            <li className='submenu__item'>handloom</li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li className='nav__item'>fabric</li>
-            <li className='nav__item'>journal</li>
-            <li className='nav__item'>about</li>
-        </ul>
-        <div></div>
+        <div className='info'>Free shipping for orders above USD 150</div>
+        <div className='navigation'>
+            <span className='logo'></span>
+            <ul className='nav'>
+                <li className='nav__item one'>shop
+                    <ul className='menu'>
+                        <li className='menu-item'>Apparels
+                            <ul className='submenu'>
+                                <li className='submenu__item'>pants</li>
+                                <li className='submenu__item'>jumpsuits</li>
+                                <li className='submenu__item'>shorts</li>
+                                <li className='submenu__item'>tops</li>
+                            </ul>
+                        </li>
+                        <li className='menu-item'>Accessories
+                            <ul className='submenu'>
+                                <li className='submenu__item'>necklaces</li>
+                                <li className='submenu__item'>scarfs</li>
+                            </ul>
+                        </li>
+                        <li className='menu-item'>Other
+                            <ul className='submenu'>
+                                <li className='submenu__item'>houseware</li>
+                                <li className='submenu__item'>gift cards</li>
+                            </ul>
+                        </li>
+                        <li className='menu-item'>Shop by collection
+                            <ul className='submenu'>
+                                <li className='submenu__item'>sale/Last Call</li>
+                                <li className='submenu__item'>essentials</li>
+                            </ul>
+                        </li>
+                        <li className='menu-item'>Shop by techniques
+                            <ul className='submenu'>
+                                <li className='submenu__item'>block Printing</li>
+                                <li className='submenu__item'>ikat</li>
+                                <li className='submenu__item'>jamdani</li>
+                                <li className='submenu__item'>handloom</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li className='nav__item two'>fabric</li>
+                <li className='nav__item three'>journal</li>
+                <li className='nav__item four'>about</li>
+            </ul>
+            <div className='personal'>
+                <div className='profile'>
+                    <span className='user'>welcom, Alex</span>
+                    <span className='avatar'></span>
+                </div>
+                <button className='button search'></button>
+                <button className='button likes'></button>
+                <button className='button bag'></button>
+            </div>
+        </div>
     </div>
 }
 
