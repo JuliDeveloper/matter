8355233fb89f6fc916bd927ec6a316df3219736a
Julia Romanenko <rja-97@yandex.ru>
2018-12-18 11:53:23 +0300

    created new branch and new component

--- matter/src/App.jsx
+++ matter/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 import './reset-css.css';
 import './App.css';
+import Header from './component/Header.jsx';
 
 function App() {
   return <Fragment>
-
+      <Header />
   </Fragment>
 }
 
