f5293b9dde8ec3a482a995fe92fa5cd213067d90
Julia Romanenko <rja-97@yandex.ru>
2018-12-19 18:19:38 +0300

    created new branch and new component

--- matter/src/App.jsx
+++ matter/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 import './reset-css.css';
 import './App.css';
+import Featured from './component/Featured.jsx';
 
 function App() {
   return <Fragment className="main">
-
+      <Featured />
   </Fragment>
 }
 
---
+++ matter/src/component/Featured.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Feature.css';
+
+function Feature() {
+    return <div>
+
+    </div>
+}
+
+export default Feature;
